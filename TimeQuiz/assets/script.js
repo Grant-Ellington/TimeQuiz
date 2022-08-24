9e40a01b8f95b600ae58ac7b03c9db83bcdd0437
Grant-Ellington <gellingtonem6@gmail.com>
2022-08-24 14:14:20 -0500

    added question file

--- TimeQuiz/assets/script.js
+++ TimeQuiz/assets/script.js
@@ -1,93 +1,189 @@
-// var- object that holds the questions
-var questions = [
-    {
-        title: "What is Norma's favorite toy",
-        multiChoice: ["the bird", "the mouse", "the treat leaf", "the worm"]
-        answer: "the bird"
-    }
-]
-
-// start timer variable
-
+// a variable for start time
 let secondsLeft = 76;
 
-//element that displays the time
+//the element that displays the time
+let timer = document.getElementById("timer");
 
-let timer = document.getElementById('timer');
+//div for high scores
+let scoresDiv = document.getElementById("scores-div");
 
-//div for high Score
-let scoresDiv = document.getElementById('score-div');
+let buttonsDiv = document.getElementById("buttons")
 
-let buttondiv = div.getElementById('button');
+//button for high scores
+let viewScoresBtn = document.getElementById("view-scores")
 
-//button for high score
+//start button div
+let startButton = document.getElementById("start-button");
+startButton.addEventListener("click", setTime);
 
-let viewScoreBtn = document.getElementById('view-score');
 
-//div to hold results
+// variable for the questions title
+var questionDiv = document.getElementById("question-div");
 
-let results = document.getElementById('results');
+// div to hold the results
+let results = document.getElementById("results");
 
-//div for choices 
+// div for the choices
+var choices = document.getElementById("choices");
 
-let choices = document.getElementById('choices');
 
-//an array which store the high score
+// an array to store high scores
+let emptyArray = [];
 
-let emptyArray = JSON.parse(window.localStorage.getItem('highScores'));
+// the array of high scores from local storage
+let storedArray = JSON.parse(window.localStorage.getItem("highScores"));
 
-//track questions
-
+// keeping track of which question we're on
 var questionCount = 0;
 
-//score that is tracked
+//keeping score
+let score = 0
 
-let score = 0;
-
-
-//removes uneeded elements. '...' copies the els; thus it is checking all els.; el can geneerate dom elements of be a place holder for dom elements; therefore el is being inacted on any dom element 
-const removeEls = (...els) => {
-    for(let el of els)el.remove();
+//Timer starts when the user clicks startButton (see above).
+function setTime() {
+  displayQuestions();
+  let timerInterval = setInterval(function() {
+    secondsLeft--;
+    timer.textContent = "";
+    timer.textContent = "Time: " + secondsLeft;
+    if (secondsLeft <= 0 || questionCount === questions.length) {
+      clearInterval(timerInterval);
+      captureUserScore();
+    } 
+  }, 1000);
 }
 
-// fuction to set timer
+//function to load the questions on the page
+function displayQuestions() {
+  removeEls(startButton);
 
-function setTimer() {
-    
-}
-// This fuction dispplays the questions.
-function diplayQuestions () {
-    removeEls(startButton);
+  if (questionCount < questions.length) {
+    questionDiv.innerHTML = questions[questionCount].title;
+    choices.textContent = "";
 
-    if(quiestionCount < questions.length) {            //If the questions gone throyugh tracked through question count is less than the number fo questions-tracked through the question,length parameter
-        questionDiv.innerHTML = questions[questionCount].title;
-        choices.textContent = ""; 
-        // this creates the buttons within the questions via the question object. muti choice
-        for(let i = 0; i<questions[questionCount].multiChoice.length; i++) {
-            let el =document.createElement('button');.
-            el.innerText = questions[questionCount].multiChoice[i]; // fills in the text for the buttton
-            el.addEventListener("click", function (event) {
-                event.stopPropagation();
-                // below checks if the question answer is correct and determines the path of wether to go to the next question or  ove away from it.
-                if (el.innerText === questions[questionCount].answer) {
-                    score += secondsLeft;
-                }else {
-                    score -= 10;
-                    secondsLeft = secondsLeft -15;
-                }
-                questionDiv.innerHTML = '';
+    for (let i = 0; i < questions[questionCount].multiChoice.length; i++) {
+      let el = document.createElement("button");
+      el.innerText = questions[questionCount].multiChoice[i];
+      el.setAttribute("data-id", i);
+      el.addEventListener("click", function (event) {
+        event.stopPropagation();
 
-                if(questioncount === questionCount.length) {
-                    return;
-                }   else {
-                    questionCount++;
-                    displayQuestion();
-            }
-            });
+        if (el.innerText === questions[questionCount].answer) {
+          score += secondsLeft;
+        } else {
+          score -= 10;
+          secondsLeft = secondsLeft - 15;
         }
-    
-    
-    }
+        
+        questionDiv.innerHTML = "";
 
+        if (questionCount === questions.length) {
+          return;
+        } else {
+          questionCount++;
+          displayQuestions();
+        }
+      });
+      choices.append(el);
+    }
+  }
 }
 
+
+function captureUserScore() {
+  timer.remove();
+  choices.textContent = "";
+
+  let initialsInput = document.createElement("input");
+  let postScoreBtn = document.createElement("input");
+
+  results.innerHTML = `You scored ${score} points! Enter initials: `;
+  initialsInput.setAttribute("type", "text");
+  postScoreBtn.setAttribute("type", "button");
+  postScoreBtn.setAttribute("value", "Post My Score!");
+  postScoreBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    let scoresArray = defineScoresArray(storedArray, emptyArray);
+
+    let initials = initialsInput.value;
+    let userAndScore = {
+      initials: initials,
+      score: score,
+    };
+
+    scoresArray.push(userAndScore);
+    saveScores(scoresArray);
+    displayAllScores();
+    clearScoresBtn();
+    goBackBtn();
+    viewScoresBtn.remove();
+  });
+  results.append(initialsInput);
+  results.append(postScoreBtn);
+}
+
+const saveScores = (array) => {
+  window.localStorage.setItem("highScores", JSON.stringify(array));
+}
+
+const defineScoresArray = (arr1, arr2) => {
+  if(arr1 !== null) {
+    return arr1
+  } else {
+    return arr2
+  }
+}
+
+const removeEls = (...els) => {
+  for (let el of els) el.remove();
+}
+
+function displayAllScores() {
+  removeEls(timer, startButton, results);
+  let scoresArray = defineScoresArray(storedArray, emptyArray);
+
+  scoresArray.forEach(obj => {
+    let initials = obj.initials;
+    let storedScore = obj.score;
+    let resultsP = document.createElement("p");
+    resultsP.innerText = `${initials}: ${storedScore}`;
+    scoresDiv.append(resultsP);
+  });
+}
+
+function viewScores() {
+  viewScoresBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    removeEls(timer, startButton);
+    displayAllScores();
+    removeEls(viewScoresBtn);
+    clearScoresBtn();
+    goBackBtn();
+  });
+}
+
+function clearScoresBtn() {    
+  let clearBtn = document.createElement("input");
+  clearBtn.setAttribute("type", "button");
+  clearBtn.setAttribute("value", "Clear Scores");
+  clearBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    removeEls(scoresDiv);
+    window.localStorage.removeItem("highScores");
+  })
+  scoresDiv.append(clearBtn)
+}
+
+function goBackBtn() {
+  let backBtn = document.createElement("input");
+  backBtn.setAttribute("type", "button");
+  backBtn.setAttribute("value", "Go Back");
+  backBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    window.location.reload();
+  })
+  buttonsDiv.append(backBtn)
+}
+
+
+viewScores();
